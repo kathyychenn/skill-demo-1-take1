7a5675b039960bc49550167fce3473a5b7f49c6d
kathyychenn <97640717+kathyychenn@users.noreply.github.com>
2022-02-03 16:48:02 -0800

    update skilldemo.java

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,5 +1,5 @@
 public class SkillDemo{
     public static String birthday(int age){
-        return "happy " + age "th birthday!";
+        return "happy " + age + "th birthday!";
     }
 } //will type later in vs code
