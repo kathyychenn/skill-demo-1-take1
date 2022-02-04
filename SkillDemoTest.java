efb4d3eb1d65a47d2aebac402bd97f4e7e5e3fda
kathyychenn <97640717+kathyychenn@users.noreply.github.com>
2022-02-03 16:49:03 -0800

    update skilldemotest to succeed

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void test1(){
-        assertEquals("happy 49th birthday", SkillDemo.birthday(50));
+        assertEquals("happy 49th birthday", SkillDemo.birthday(49));
     }
 }
